fc14d61e0421f0c10629c2458011ded1a842a989
Skeletron72 <andrewche2003@gmail.com>
2026-09-11 12:38:15 +0300

    Move smoothing rules after corridor carving to fix 1-tile thick walls

--- scripts/components/dungeon_generator.gd
+++ scripts/components/dungeon_generator.gd
@@ -31,55 +31,12 @@ func generate(
 		child.queue_free()
 
 	# Размеры залов случайные и могут быть больше
-	var base_w = 30 + (floor_num * 2) + (randi() % 16)
-	var base_h = int(base_w * 0.75)
+	var w: int = 34 + (floor_num * 2) + (randi() % 16)
+	var h: int = int(w * 0.75)
 	
-	if base_w > 64: base_w = 64
-	if base_h > 48: base_h = 48
+	if w > 64: w = 64
+	if h > 48: h = 48
 	
-	# Делаем четными для идеального скейла
-	var w: int = base_w - (base_w % 2)
-	var h: int = base_h - (base_h % 2)
-	
-	# Генерируем пещеру в 2 раза меньшем разрешении, чтобы после увеличения x2 
-	# ВСЕ проходы были минимум 2 тайла, а любые выступы стен были минимум 2х2 тайла (без резких углов)
-	var sw = w / 2
-	var sh = h / 2
-	
-	var s_grid: Array = []
-	for x in range(sw):
-		var col = []
-		col.resize(sh)
-		col.fill(1)
-		s_grid.append(col)
-		
-	for x in range(2, sw - 2):
-		for y in range(2, sh - 2):
-			if randf() > 0.42:
-				s_grid[x][y] = 0
-				
-	for i in range(4):
-		var new_s = s_grid.duplicate(true)
-		for x in range(1, sw - 1):
-			for y in range(1, sh - 1):
-				var walls = 0
-				for dx in range(-1, 2):
-					for dy in range(-1, 2):
-						if s_grid[x + dx][y + dy] == 1:
-							walls += 1
-				if walls >= 5:
-					new_s[x][y] = 1
-				elif walls <= 3:
-					new_s[x][y] = 0
-		s_grid = new_s
-		
-	var scx = sw / 2
-	var scy = sh / 2
-	for dx in range(-1, 2):
-		for dy in range(-1, 2):
-			s_grid[scx + dx][scy + dy] = 0
-			
-	# Увеличиваем в 2 раза
 	var grid: Array = []
 	for x in range(w):
 		var col = []
@@ -87,12 +44,33 @@ func generate(
 		col.fill(1)
 		grid.append(col)
 		
-	for x in range(w):
-		for y in range(h):
-			grid[x][y] = s_grid[x / 2][y / 2]
-			
+	for x in range(3, w - 3):
+		for y in range(4, h - 4):
+			if randf() > 0.45:
+				grid[x][y] = 0
+				
+	for i in range(5):
+		var new_grid = grid.duplicate(true)
+		for x in range(2, w - 2):
+			for y in range(3, h - 3):
+				var walls = 0
+				for dx in range(-1, 2):
+					for dy in range(-1, 2):
+						if grid[x + dx][y + dy] == 1:
+							walls += 1
+				if walls >= 5:
+					new_grid[x][y] = 1
+				elif walls <= 3:
+					new_grid[x][y] = 0
+		grid = new_grid
+		
+
+
 	var cx = w / 2
 	var cy = h / 2
+	for dx in range(-3, 4):
+		for dy in range(-3, 4):
+			grid[cx + dx][cy + dy] = 0
 
 	# Carve a dedicated 5-tile wide vertical corridor for the support
 	var support_placed = true
@@ -107,6 +85,29 @@ func generate(
 	for dx in range(-3, 3):
 		grid[cx + dx][cy] = 0
 		grid[cx + dx][cy - 12] = 0
+
+	# Убираем стены толщиной в 1 тайл
+	for x in range(1, w - 1):
+		for y in range(1, h - 1):
+			if grid[x][y] == 1:
+				if grid[x-1][y] == 0 and grid[x+1][y] == 0: grid[x][y] = 0
+				if grid[x][y-1] == 0 and grid[x][y+1] == 0: grid[x][y] = 0
+				
+	# Убираем коридоры шириной в 1 тайл (расширяем их)
+	for i in range(2):
+		for x in range(1, w - 1):
+			for y in range(1, h - 1):
+				if grid[x][y] == 0:
+					if grid[x-1][y] == 1 and grid[x+1][y] == 1: grid[x+1][y] = 0
+					if grid[x][y-1] == 1 and grid[x][y+1] == 1: grid[x][y+1] = 0
+					
+	# Сглаживаем "лесенки" (диагонально касающиеся углы стен)
+	for x in range(1, w - 1):
+		for y in range(1, h - 1):
+			if grid[x][y] == 1 and grid[x+1][y+1] == 1 and grid[x+1][y] == 0 and grid[x][y+1] == 0:
+				grid[x+1][y] = 1
+			if grid[x+1][y] == 1 and grid[x][y+1] == 1 and grid[x][y] == 0 and grid[x+1][y+1] == 0:
+				grid[x][y] = 1
 
 
 	var floor_cells: Array[Vector2i] = []
