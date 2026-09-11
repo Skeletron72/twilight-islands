7a8b6c2b9b61cb8a712cd3375c321cdda6564df1
Skeletron72 <andrewche2003@gmail.com>
2026-09-11 12:43:03 +0300

    Use correct texture for support, restore exactly the corner mapping approved by the user

--- scripts/components/dungeon_generator.gd
+++ scripts/components/dungeon_generator.gd
@@ -141,11 +141,11 @@ func generate(
 				
 				# ПРАВИЛЬНЫЙ МАППИНГ ДЛЯ RPG MAKER 3x3 (ВЕРШИНА ГОРЫ)
 				
-				# Внешние углы (Правильный маппинг для 2x2 блоков, темная сторона наружу)
-				if f_n and f_w: tile = Vector2i(6, 2)
-				elif f_n and f_e: tile = Vector2i(4, 2)
-				elif f_s and f_w: tile = Vector2i(6, 0)
-				elif f_s and f_e: tile = Vector2i(4, 0)
+				# Внешние углы (Теперь используем тайлы внутренних углов пустоты!)
+				if f_n and f_w: tile = Vector2i(4, 3)
+				elif f_n and f_e: tile = Vector2i(5, 3)
+				elif f_s and f_w: tile = Vector2i(4, 4)
+				elif f_s and f_e: tile = Vector2i(5, 4)
 				
 				# Прямые края
 				elif f_n: tile = Vector2i(5, 2)
@@ -153,11 +153,11 @@ func generate(
 				elif f_w: tile = Vector2i(6, 1)
 				elif f_e: tile = Vector2i(4, 1)
 				
-				# Внутренние углы (впадины в скале)
-				elif f_nw: tile = Vector2i(4, 3)
-				elif f_ne: tile = Vector2i(5, 3)
-				elif f_sw: tile = Vector2i(4, 4)
-				elif f_se: tile = Vector2i(5, 4)
+				# Внутренние углы (Поменяли местами по диагонали!)
+				elif f_nw: tile = Vector2i(6, 2)
+				elif f_ne: tile = Vector2i(4, 2)
+				elif f_sw: tile = Vector2i(6, 0)
+				elif f_se: tile = Vector2i(4, 0)
 				
 				if tile != Vector2i(-1, -1):
 					wall_layer.set_cell(Vector2i(x, y), SOURCE_WALLS, tile)
@@ -177,7 +177,8 @@ func generate(
 						face_top = Vector2i(2, 6)
 						face_bot = Vector2i(2, 7)
 						
-					# Inner corners
+					# Inner corners (swapped)
+					# f_se -> (4,0) (Inner BR). It has a small south face on the left.
 					if f_se and not f_s:
 						face_top = Vector2i(2, 6)
 						face_bot = Vector2i(2, 7)
