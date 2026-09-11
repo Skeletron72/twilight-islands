9c591991b131aeee2cc379751daae39b00dcee5a
Skeletron72 <andrewche2003@gmail.com>
2026-09-11 12:55:56 +0300

    Fix CaveSupport collision, y-sorting and side walls generation

--- scripts/components/dungeon_generator.gd
+++ scripts/components/dungeon_generator.gd
@@ -80,6 +80,11 @@ func generate(
 	for dy in range(-12, 0):
 		for dx in range(-2, 2):
 			grid[cx + dx][cy + dy] = 0
+		# Гарантируем, что по бокам от коридора есть толстые стены (минимум 2 тайла), чтобы они не удалились сглаживанием!
+		grid[cx - 3][cy + dy] = 1
+		grid[cx - 4][cy + dy] = 1
+		grid[cx + 2][cy + dy] = 1
+		grid[cx + 3][cy + dy] = 1
 	
 	# Плавный переход краев
 	for dx in range(-3, 3):
